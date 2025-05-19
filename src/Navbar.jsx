35a488017ccd0b821f1d648b331d120e7690a1a1
m0skwa <michael_zaharevic@tutanota.de>
2025-05-19 21:37:20 +0200

    Responsive Design

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,9 @@
-import './Navbar.css'
-import { Link } from "@chakra-ui/react"
-import React, { useState, useEffect } from 'react';
+import "./Navbar.css";
+import { Link, Button, Menu, Portal } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function Navbar() {
-
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
@@ -16,25 +16,66 @@ function Navbar() {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
-    <header className={`navbar ${scrolled ? 'scrolled' : ''}`} >
+    <header className={`navbar ${scrolled ? "scrolled" : ""}`}>
       <a href="#landing">
         <h1>m0skwa</h1>
       </a>
       <ul className="list-items">
-        <li><Link className="item" href="#about">About</Link></li>
-        <li><Link className="item" href="#skills">Skills</Link></li>
-        <li><Link className="item" href="#projects">Projects</Link></li>
+        <li>
+          <Link className="item" href="#about">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link className="item" href="#skills">
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link className="item" href="#projects">
+            Projects
+          </Link>
+        </li>
       </ul>
+
+      <Menu.Root>
+        <Menu.Trigger asChild>
+          <Button variant="ghost" size="sm" className="burgerMenu">
+            <GiHamburgerMenu />
+          </Button>
+        </Menu.Trigger>
+        <Portal>
+          <Menu.Positioner>
+            <Menu.Content>
+              <Menu.Item>
+                <Link className="item" href="#about">
+                  About
+                </Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link className="item" href="#skills">
+                  Skills
+                </Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link className="item" href="#projects">
+                  Projects
+                </Link>
+              </Menu.Item>
+            </Menu.Content>
+          </Menu.Positioner>
+        </Portal>
+      </Menu.Root>
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
